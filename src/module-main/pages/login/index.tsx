450c755477de92f181f7ef3aef059690dac20ad2
Trần Xuân Lộc <loctx@tinhvan.com>
2023-02-13 13:55:45 +0700

    update

--- src/module-main/pages/login/index.tsx
+++ src/module-main/pages/login/index.tsx
@@ -54,60 +54,60 @@ const Login = () => {
 
   return <>
 
-    <LoginHead />
-    <div style={{ background: '#ececf1', height: 'calc(100vh - 273px)' }}>
-      <LoginStyleGl.SCGrid >
-        <LoginStyleGl.SCRow >
-          <LoginStyleGl.SCCol_5 >
-            <ContentLogin />
-          </LoginStyleGl.SCCol_5>
-          <LoginStyleGl.SCCol_4 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <LoginStyle.App>
-              <LoginStyle.Tittle>Login</LoginStyle.Tittle>
-              <form className="form" onSubmit={handleSubmit(onSave)}>
-                <div className="input-group">
-                  <LoginStyle.IputLogin error={errors.email} type="text" {...register("email", { required: true,
-                  pattern: {
-                    value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-                    message: "Email format is wrong"
-                  },})} placeholder="Enter Username"
-                  />
-                  <Errors errors={errors} name="email" />
-                </div>
-                <div className="input-group">
-                  <LoginStyle.IputLogin error={errors.password} type="password" {...register("password", { required: true,
-                  minLength: {
-                    value: 8,
-                    message: "Password must exceed 8 characters"
-                  } })} placeholder="Enter Password" />
-                  <Errors errors={errors} name="password" />
-                </div>
-                <div className="keep-signin">
-                  <div>
-                    <input onChange={(e) => setRemember(!remember)}  type="checkbox" id="checkbox-1-1" className="custom-checkbox" />
-                    <label htmlFor="checkbox-1-1" className="keep-me">Keep Me Signed In</label>
+    <div style={{ background: '#ececf1', height: '100vh' }}>
+      <LoginHead />
+        <LoginStyleGl.SCGrid >
+          <LoginStyleGl.SCRow >
+            <LoginStyleGl.SCCol_5 >
+              <ContentLogin />
+            </LoginStyleGl.SCCol_5>
+            <LoginStyleGl.SCCol_4 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <LoginStyle.App>
+                <LoginStyle.Tittle>Login</LoginStyle.Tittle>
+                <form className="form" onSubmit={handleSubmit(onSave)}>
+                  <div className="input-group">
+                    <LoginStyle.IputLogin error={errors.email} type="text" {...register("email", { required: true,
+                    pattern: {
+                      value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                      message: "Email format is wrong"
+                    },})} placeholder="Enter Username"
+                    />
+                    <Errors errors={errors} name="email" />
                   </div>
-
-                  <div className="changepw">
-                    <Link to={"/forgot-password"} className="text-auth"> Forgot Password ? </Link>
+                  <div className="input-group">
+                    <LoginStyle.IputLogin error={errors.password} type="password" {...register("password", { required: true,
+                    minLength: {
+                      value: 8,
+                      message: "Password must exceed 8 characters"
+                    } })} placeholder="Enter Password" />
+                    <Errors errors={errors} name="password" />
                   </div>
-                </div>
-                <LoginStyle.SCButtonLogin className="primary" >Submit
+                  <div className="keep-signin">
+                    <div>
+                      <input onChange={(e) => setRemember(!remember)}  type="checkbox" id="checkbox-1-1" className="custom-checkbox" />
+                      <label htmlFor="checkbox-1-1" className="keep-me">Keep Me Signed In</label>
+                    </div>
 
-                  {/* <div className="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="15.582" height="15.582" viewBox="0 0 15.582 15.582">
-                      <path id="Icon_ionic-md-arrow-forward" data-name="Icon ionic-md-arrow-forward" d="M5.977,14.741H17.809l-5.454,5.454,1.412,1.363,7.791-7.791L13.767,5.977,12.4,7.34l5.4,5.454H5.977Z" transform="translate(-5.977 -5.977)" fill="#575f6b" />
-                    </svg>
-                  </div> */}
-                </LoginStyle.SCButtonLogin >
-                <div className="register">
-                  <Link to={"/register"} className="text-auth"> Don't have an account yet? Singup Now</Link>
-                </div>
-              </form>
-            </LoginStyle.App>
-          </LoginStyleGl.SCCol_4>
-        </LoginStyleGl.SCRow>
-      </LoginStyleGl.SCGrid>
+                    <div className="changepw">
+                      <Link to={"/forgot-password"} className="text-auth"> Forgot Password ? </Link>
+                    </div>
+                  </div>
+                  <LoginStyle.SCButtonLogin className="primary" >Submit
+
+                    {/* <div className="icon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="15.582" height="15.582" viewBox="0 0 15.582 15.582">
+                        <path id="Icon_ionic-md-arrow-forward" data-name="Icon ionic-md-arrow-forward" d="M5.977,14.741H17.809l-5.454,5.454,1.412,1.363,7.791-7.791L13.767,5.977,12.4,7.34l5.4,5.454H5.977Z" transform="translate(-5.977 -5.977)" fill="#575f6b" />
+                      </svg>
+                    </div> */}
+                  </LoginStyle.SCButtonLogin >
+                  <div className="register">
+                    <Link to={"/register"} className="text-auth"> Don't have an account yet? Singup Now</Link>
+                  </div>
+                </form>
+              </LoginStyle.App>
+            </LoginStyleGl.SCCol_4>
+          </LoginStyleGl.SCRow>
+        </LoginStyleGl.SCGrid>
     </div>
 
   </>
